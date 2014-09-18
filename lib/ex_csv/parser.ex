e8100a9de444dabf404701afa93ba7aaa8dd1453
Bruce Williams <brwcodes@gmail.com>
2014-09-18 01:32:01 -0700

    Support for partial quoting, unclosed quote detection

--- lib/ex_csv/parser.ex
+++ lib/ex_csv/parser.ex
@@ -1,64 +1,80 @@
 defmodule ExCsv.Parser do
-  defstruct delimiter: 44, newline: 10, quote: 34, quoting: false
+  defstruct delimiter: 44, newline: 10, quote: 34, quoting: false, eat_next_quote: true
 
   def parse(text) do
     parse(text, %ExCsv.Parser{})
   end
-  def parse(iodata, settings) when is_list(iodata) do
-    iodata |> IO.iodata_to_binary |> parse(settings)
+  def parse(iodata, config) when is_list(iodata) do
+    iodata |> IO.iodata_to_binary |> parse(config)
   end
-  def parse(string, settings) when is_binary(string) do
-    string |> skip_whitespace |> build([[]], settings)
-           |> Enum.reverse |> Enum.map &(Enum.reverse(&1))
+  def parse(string, config) when is_binary(string) do
+    {result, state} = string |> skip_whitespace |> build([[]], config)
+    if state.quoting do
+      {:error, "quote meets end of file"}
+    else
+      {:ok, result |> Enum.reverse |> Enum.map &(Enum.reverse(&1))}
+    end
   end
 
   # DELIMITER
   # At the beginning of a row
-  defp build(<<char>> <> rest, [[] | previous_rows], %{delimiter: char, quoting: false} = settings) do
+  defp build(<<char>> <> rest, [[] | previous_rows], %{delimiter: char, quoting: false} = config) do
     current_row = [new_field, new_field]
     rows = [current_row | previous_rows]
-    rest |> skip_whitespace |> build(rows, settings)
+    rest |> skip_whitespace |> build(rows, config)
   end
   # After the beginning of a row
-  defp build(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], %{delimiter: char, quoting: false} = settings) do
+  defp build(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], %{delimiter: char, quoting: false} = config) do
     current_row = [new_field | [current_field |> String.rstrip | previous_fields]]
     rows = [current_row | previous_rows]
-    rest |> skip_whitespace |> build(rows, settings)
+    rest |> skip_whitespace |> build(rows, config)
   end
 
   # QUOTE
-  # Start quote
-  defp build(<<char>> <> rest, rows, %{quote: char, quoting: false} = settings) do
-    rest |> build(rows, %{ settings | quoting: true })
+  # Start quote at the beginning of a field (don't retain this quote pair)
+  defp build(<<char>> <> rest, [["" | _previous_fields] | _previous_rows] = rows, %{quote: char, quoting: false} = config) do
+    rest |> build(rows, %{ config | quoting: true, eat_next_quote: true })
   end
-  # End quote
-  defp build(<<char>> <> rest, rows, %{quote: char, quoting: true} = settings) do
-    rest |> build(rows, %{ settings | quoting: false })
+  # Start quote in the middle of a field (retain this quote pair)
+  defp build(<<char>> <> rest,  [[current_field | previous_fields] | previous_rows], %{quote: char, quoting: false} = config) do
+    current_row = [current_field <> <<char::utf8>> | previous_fields]
+    rows = [current_row | previous_rows]
+    rest |> build(rows, %{ config | quoting: true, eat_next_quote: false })
+  end
+  # End quote and don't retain the quote character (full-field quoting)
+  defp build(<<char>> <> rest, rows, %{quote: char, quoting: true, eat_next_quote: true} = config) do
+    rest |> skip_whitespace |> build(rows, %{ config | quoting: false })
+  end
+  # End quote and retain the quote character (partial field quoting)
+  defp build(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], %{quote: char, quoting: true, eat_next_quote: false} = config) do
+    current_row = [current_field <> <<char::utf8>> | previous_fields]
+    rows = [current_row | previous_rows]
+    rest |> build(rows, %{ config | quoting: false })
   end
 
   # NEWLINE
-  defp build(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], %{newline: char} = settings) do
+  defp build(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], %{newline: char} = config) do
     current_row = [current_field |> String.rstrip | previous_fields]
     rows = [new_row | [current_row | previous_rows]]
-    rest |> skip_whitespace |> build(rows, settings)
+    rest |> skip_whitespace |> build(rows, config)
   end
 
   # NORMAL CHARACTER
   # Starting the first field in the current row
-  defp build(<<char>> <> rest, [[] | previous_rows], settings) do
+  defp build(<<char>> <> rest, [[] | previous_rows], config) do
     current_row = [<<char::utf8>>]
     rows = [current_row | previous_rows]
-    rest |> build(rows, settings)
+    rest |> build(rows, config)
   end
   # Adding to the last field in the current row
-  defp build(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], settings) do
+  defp build(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], config) do
     current_row = [current_field <> <<char::utf8>> | previous_fields]
     rows = [current_row | previous_rows]
-    rest |> build(rows, settings)
+    rest |> build(rows, config)
   end
 
   # EOF
-  defp build("", rows, _settings), do: rows
+  defp build("", rows, config), do: {rows, config}
 
   defp skip_whitespace(<<char>> <> rest) when char in '\s\r' do
     skip_whitespace(rest)
--- test/lib/ex_csv/parser_test.exs
+++ test/lib/ex_csv/parser_test.exs
@@ -2,37 +2,46 @@ defmodule ExCsv.ParserTest do
   use ExUnit.Case
 
   test "one simple line" do
-    assert ExCsv.Parser.parse(~s<a,b,c>) == [["a", "b", "c"]]
+    assert ExCsv.Parser.parse(~s<a,b,c>) == {:ok, [["a", "b", "c"]]}
   end
   test "one simple line with a space in a field" do
-    assert ExCsv.Parser.parse(~s<a,ba t,c>) == [["a", "ba t", "c"]]
+    assert ExCsv.Parser.parse(~s<a,ba t,c>) == {:ok, [["a", "ba t", "c"]]}
   end
   test "one simple line with a quoted field containing the delimiter" do
-    assert ExCsv.Parser.parse(~s<a,"ba,t",c>) == [["a", "ba,t", "c"]]
+    assert ExCsv.Parser.parse(~s<a,"ba,t",c>) == {:ok, [["a", "ba,t", "c"]]}
+  end
+  test "one simple line with a quoted field containing the delimiter followed by whitespace" do
+    assert ExCsv.Parser.parse(~s<a,"ba,t" ,c>) == {:ok, [["a", "ba,t", "c"]]}
+  end
+  test "one simple line with a quoted portion of a field containing the delimiter" do
+    assert ExCsv.Parser.parse(~s<a,he said "ok, there",c>) == {:ok, [["a", ~s<he said "ok, there">, "c"]]}
+  end
+  test "one simple line with a quoted field that is not finished" do
+    assert {:error, _} = ExCsv.Parser.parse(~s<a,"ba,t,c>)
   end
   test "one simple line that starts with a delimiter" do
-    assert ExCsv.Parser.parse(~s<,a,b,c>) == [["", "a", "b", "c"]]
+    assert ExCsv.Parser.parse(~s<,a,b,c>) == {:ok, [["", "a", "b", "c"]]}
   end
   test "one simple line that ends with a delimiter" do
-    assert ExCsv.Parser.parse(~s<a,b,c,>) == [["a", "b", "c", ""]]
+    assert ExCsv.Parser.parse(~s<a,b,c,>) == {:ok, [["a", "b", "c", ""]]}
   end
   test "one simple line with post-delimiter whitespace" do
-    assert ExCsv.Parser.parse(~s<a,b, c>) == [["a", "b", "c"]]
+    assert ExCsv.Parser.parse(~s<a,b, c>) == {:ok, [["a", "b", "c"]]}
   end
   test "one simple line with pre-delimiter whitespace" do
-    assert ExCsv.Parser.parse(~s<a,b ,c>) == [["a", "b", "c"]]
+    assert ExCsv.Parser.parse(~s<a,b ,c>) == {:ok, [["a", "b", "c"]]}
   end
 
   test "two simple lines" do
-    assert ExCsv.Parser.parse(~s<a,b,c\nd,e,f>) == [["a", "b", "c"], ["d", "e", "f"]]
+    assert ExCsv.Parser.parse(~s<a,b,c\nd,e,f>) == {:ok, [["a", "b", "c"], ["d", "e", "f"]]}
   end
   test "two simple lines with pre-newline whitespace" do
-    assert ExCsv.Parser.parse(~s<a,b,c \nd,e,f>) == [["a", "b", "c"], ["d", "e", "f"]]
+    assert ExCsv.Parser.parse(~s<a,b,c \nd,e,f>) == {:ok, [["a", "b", "c"], ["d", "e", "f"]]}
   end
   test "two simple lines with post-newline whitespace" do
-    assert ExCsv.Parser.parse(~s<a,b,c\n d,e,f>) == [["a", "b", "c"], ["d", "e", "f"]]
+    assert ExCsv.Parser.parse(~s<a,b,c\n d,e,f>) == {:ok, [["a", "b", "c"], ["d", "e", "f"]]}
   end
   test "two simple lines with second starting with a delimiter" do
-    assert ExCsv.Parser.parse(~s<a,b,c\n,e,f>) == [["a", "b", "c"], ["", "e", "f"]]
+    assert ExCsv.Parser.parse(~s<a,b,c\n,e,f>) == {:ok, [["a", "b", "c"], ["", "e", "f"]]}
   end
 end
