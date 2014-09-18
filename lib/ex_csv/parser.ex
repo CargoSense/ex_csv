6df668adf24d14f45cbd458681f02bfa5d85a67a
Bruce Williams <brwcodes@gmail.com>
2014-09-18 00:47:28 -0700

    Consistent current_row, row assignments, pipe more things

--- lib/ex_csv/parser.ex
+++ lib/ex_csv/parser.ex
@@ -8,44 +8,54 @@ defmodule ExCsv.Parser do
     iodata |> IO.iodata_to_binary |> parse(settings)
   end
   def parse(string, settings) when is_binary(string) do
-    read(skip_whitespace(string), [[]], settings)
-    |> Enum.reverse |> Enum.map &(Enum.reverse(&1))
+    string |> skip_whitespace |> read([[]], settings)
+           |> Enum.reverse |> Enum.map &(Enum.reverse(&1))
   end
 
-  # Delimiter at the beginning of a row
+  # DELIMITER
+  # At the beginning of a row
   defp read(<<char>> <> rest, [[] | previous_rows], %{delimiter: char} = settings) do
-    rows = [["", ""] | previous_rows]
+    current_row = [new_field, new_field]
+    rows = [current_row | previous_rows]
     rest |> skip_whitespace |> read(rows, settings)
   end
-  # Delimiter after the beginning of a row
+  # After the beginning of a row
   defp read(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], %{delimiter: char} = settings) do
-    rows = [["" | [current_field |> String.rstrip | previous_fields]] | previous_rows]
+    current_row = [new_field | [current_field |> String.rstrip | previous_fields]]
+    rows = [current_row | previous_rows]
     rest |> skip_whitespace |> read(rows, settings)
   end
 
-  # Newline
+  # NEWLINE
   defp read(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], %{newline: char} = settings) do
-    rows = [[] | [[current_field |> String.rstrip | previous_fields] | previous_rows]]
+    current_row = [current_field |> String.rstrip | previous_fields]
+    rows = [new_row | [current_row | previous_rows]]
     rest |> skip_whitespace |> read(rows, settings)
   end
 
+  # NORMAL CHARACTER
   # Starting the first field in the current row
   defp read(<<char>> <> rest, [[] | previous_rows], settings) do
-    rows = [[<<char::utf8>>] | previous_rows]
+    current_row = [<<char::utf8>>]
+    rows = [current_row | previous_rows]
     rest |> read(rows, settings)
   end
   # Adding to the last field in the current row
   defp read(<<char>> <> rest, [[current_field | previous_fields] | previous_rows], settings) do
-    rows = [[current_field <> <<char::utf8>> | previous_fields] | previous_rows]
+    current_row = [current_field <> <<char::utf8>> | previous_fields]
+    rows = [current_row | previous_rows]
     rest |> read(rows, settings)
   end
 
-  # All done
-  defp read("", rows, settings), do: rows
+  # EOF
+  defp read("", rows, _settings), do: rows
 
   defp skip_whitespace(<<char>> <> rest) when char in '\s\r' do
     skip_whitespace(rest)
   end
   defp skip_whitespace(string), do: string
+
+  defp new_field, do: ""
+  defp new_row, do: []
 
 end
